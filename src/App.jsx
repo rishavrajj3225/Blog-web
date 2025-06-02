7283a9f3cb31d3864b1442a5c849fa10b51d8a98
Rishu raj <21je0769@iitism.ac.in>
2025-06-02 16:02:43 +0530

    backend is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import { Header,Footer,Loader } from './components/index'
+import {login,logout} from './store/authSlice'
+import { Outlet } from 'react-router-dom'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [data, setData] = useState(null)
+  const dispatch = useDispatch()
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout()); // humesha state update hi rahega
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      }); // finally is used to execute code after the promise is settled, regardless of its outcome
+  }, []);
+
+  if (loading) {
+    return <>
+    <Loader/>
+    </>
+  }
   return (
     <>
-    
+      <Header />
+      <main>
+       TODO: <Outlet />
+      </main>
+      <Footer />
     </>
-  ) 
+  )
+
+
 }
 
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Loader from "./Loader";
+
+export { Header, Footer, Loader };
